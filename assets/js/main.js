90252f4368232bd1ed2d0547ea1b18526f436631
hi991016 <miureus@gmail.com>
2024-07-13 15:31:27 +0700

    setup swiper layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,36 +2,39 @@
 
 // ===== get height app =====
 const appHeight = () => {
-    const doc = document.documentElement;
-    doc.style.setProperty(
-        "--app-height",
-        `${document.documentElement.clientHeight}px`
-    );
+  const doc = document.documentElement;
+  doc.style.setProperty(
+    "--app-height",
+    `${document.documentElement.clientHeight}px`
+  );
 };
 window.addEventListener("resize", appHeight);
 
 // ===== main =====
 const main = () => {
-    const swiperShips = new Swiper(".js-ships-swiper", {
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-        speed: 1200,
-        breakpoints: {
-            0: {
-                allowTouchMove: true,
-            },
-            1024: {
-                allowTouchMove: false,
-            },
-        },
-        on: {}
-    });
-}
+  const swiperShips = new Swiper(".js-ships-swiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    slidesPerView: "auto",
+    speed: 1200,
+    breakpoints: {
+      0: {
+        allowTouchMove: true,
+        slidesPerGroup: 1,
+      },
+      1024: {
+        allowTouchMove: false,
+        slidesPerGroup: 2,
+      },
+    },
+    on: {},
+  });
+};
 
 window.onload = () => {
-    document.body.classList.remove("fadeout");
-    appHeight();
-    main();
+  document.body.classList.remove("fadeout");
+  appHeight();
+  main();
 };
