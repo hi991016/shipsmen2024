4b94f8ff3c6ae40e9c125ab0dfdd5dabd70d6ad3
hi991016 <miureus@gmail.com>
2024-07-15 16:34:15 +0700

    coding mainvisual

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,32 +12,79 @@ window.addEventListener("resize", appHeight);
 
 // ===== main =====
 const main = () => {
-  const jsSwiper = document.querySelector(".js-ships-swiper");
+  // query
+  const sidebarControl = document.querySelector(".js-sidebar-control");
 
-  const shipsSwiper = new Swiper(jsSwiper, {
+  // init swiper
+  new Swiper(".js-mainvisual-swiper", {
+    loop: true,
+    effect: "fade",
+    allowTouchMove: false,
+    autoplay: {
+      delay: 7000,
+      disableOnInteraction: false,
+    },
+    speed: 2000,
+  });
+
+  const shipsSwiper = new Swiper(".js-ships-swiper", {
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
-    slidesPerView: "auto",
     speed: 1200,
     breakpoints: {
       0: {
         allowTouchMove: true,
+        slidesPerView: 1,
         slidesPerGroup: 1,
       },
       1024: {
         allowTouchMove: false,
+        slidesPerView: 2,
         slidesPerGroup: 2,
       },
     },
-    on: {},
+    on: {
+      init: (sw) => {
+        if (sw.realIndex === 0 && window.innerWidth < 1024) {
+          sidebarControl.classList.add("hide");
+        }
+      },
+    },
   });
+
+  // enter slide
+  const dressBtn = document.querySelector("#js-dress");
+  const casualBtn = document.querySelector("#js-casual");
+  dressBtn.addEventListener("click", () => {
+    shipsSwiper.slideNext();
+  });
+  casualBtn.addEventListener("click", () => {
+    if (window.innerWidth > 1023) {
+      shipsSwiper.slideTo(11, 1200);
+    } else {
+      shipsSwiper.slideTo(10, 1200);
+    }
+  });
+
+  // slideChange
+  if (window.innerWidth < 1024) {
+    shipsSwiper.on("slideChange", function (sw) {
+      if (sw.realIndex === 0) {
+        sidebarControl.classList.add("hide");
+      } else {
+        sidebarControl.classList.remove("hide");
+      }
+    });
+  }
 
   // back to top
   document.body.addEventListener("click", function (event) {
     if (event.target.classList.contains("js-backtotop")) {
-      shipsSwiper.slideTo(0, 1200);
+      window.innerWidth > 1023
+        ? shipsSwiper.slideTo(2, 1200)
+        : shipsSwiper.slideTo(1, 1200);
     }
   });
 };
